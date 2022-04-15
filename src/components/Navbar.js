068d6c92a196200ec5f1753e934b686bacf83cf2
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-04-15 06:32:22 +0200

    [FEAT] Passage of props to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-const Navbar = () => {
+import { connect } from "react-redux";
+const Navbar = ({ amount }) => {
   return (
     <nav>
       <div className="nav-center">
@@ -9,12 +10,15 @@ const Navbar = () => {
             <path d="M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z" />
           </svg>
           <div className="amount-container">
-            <p className="total-amount">0</p>
+            <p className="total-amount">{amount}</p>
           </div>
         </div>
       </div>
     </nav>
   );
 };
-
-export default Navbar;
+const mapStateToProps = (state) => {
+  const { amount } = state;
+  return { amount };
+};
+export default connect(mapStateToProps)(Navbar);
