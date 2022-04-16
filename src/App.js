778f9b1126ae81f9a1be3f01a5a4bf252e20b30d
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-04-16 09:28:19 +0200

    [FEAT] Code for Redux Devtool

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   return (
